726da7e21d1d0d911925e8aff9b3257c5d3e8c9f
IsaGularte <isagularte09@gmail.com>
2025-07-14 10:24:25 -0300

    atualizando a estrutura

--- frontend/formulario/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Cadastro from "./components/Cadastro";
-
-const App: React.FC = () => {
-  return <Cadastro />;
-};
-
-export default App;
-
--- src/middlewares/authAdmin.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export function onlyAdmin(req: Request, res: Response, next: NextFunction) {
-  if (!req.body|| req.body.role !== 'admin') {
-    return res.status(403).json({ message: 'Acesso restrito apenas para administradores.' });
-  }
-  next();
-}
